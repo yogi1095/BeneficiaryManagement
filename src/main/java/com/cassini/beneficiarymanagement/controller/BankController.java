81d5c0caf4ba8a7545ff31d9a117d0c92a33a692
yogi1095 <yoga.svtm@gmail.com>
2019-12-17 13:38:03 +0530

    commited by yoga for login and get bank details

--- src/main/java/com/cassini/beneficiarymanagement/controller/BankController.java
+++ src/main/java/com/cassini/beneficiarymanagement/controller/BankController.java
@@ -1,12 +1,28 @@
 package com.cassini.beneficiarymanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cassini.beneficiarymanagement.entity.Bank;
+import com.cassini.beneficiarymanagement.exception.BankNotFound;
+import com.cassini.beneficiarymanagement.service.BankService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/banks")
 public class BankController {
+	@Autowired
+	private BankService bankService;
+
+	@GetMapping("/{ifscCode}")
+	public ResponseEntity<Bank> getBankDetails(@PathVariable("ifscCode") String ifscCode) throws BankNotFound {
+		return ResponseEntity.ok().body(bankService.getBankDetails(ifscCode));
+
+	}
 
 }
---
+++ src/main/java/com/cassini/beneficiarymanagement/exception/BankNotFound.java
@@ -0,0 +1,14 @@
+package com.cassini.beneficiarymanagement.exception;
+
+public class BankNotFound extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public BankNotFound(String message) {
+		super(message);
+	}
+	
+	
+	
+
+}
